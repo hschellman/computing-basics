2f63bd829d6f07e3f9dd4a75c0363dc69bb36d3d
Heidi Schellman <heidi.schellman@oregonstate.edu>
2024-06-14 14:38:07 -0700

    get the larsoft examples to work in apptainer and at CERN

--- gitadd.sh
+++ gitadd.sh
@@ -1,5 +1,12 @@
 git add *.md
-git add _episodes/*.md
+#git add _episodes/*.md
+git add _episodes/01-introduction.md
+git add _episodes/02-storage-spaces.md
+git add _episodes/03-data-management.md
+git add _episodes/03.2-UPS.md
+git add _episodes/03.3-cvmfs.md
+git add _episodes/04-intro-art-larsoft.md
+git add _episodes/04.4-mrb.md
 git add _includes/*.html
 git add *.yml
 git add _extras/*.md
