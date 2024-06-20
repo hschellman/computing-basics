d0f026162f990aa174d59f07a04f2010c751486b
Heidi Schellman <heidi.schellman@oregonstate.edu>
2024-06-20 08:13:00 -0700

    add in justin

--- gitadd.sh
+++ gitadd.sh
@@ -7,7 +7,11 @@ git add _episodes/03.2-UPS.md
 git add _episodes/03.3-cvmfs.md
 git add _episodes/04-intro-art-larsoft.md
 git add _episodes/04.4-mrb.md
-git add _includes/*.html
+#git add _episodes/06-larsoft-modify-module.md
+git add _episodes/07-grid-job-submission.md
+git add _episodes/08-submit-jobs-w-justin.md
+git add _episodes/09-grid-batch-debug.md
+#git add _episodes/10-closing-remarks.mdgit add _includes/*.html
 git add *.yml
 git add _extras/*.md
 git add AUTHORS CITATION 
