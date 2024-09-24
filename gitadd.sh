700d3ce017202cc79341744250ff25fc7d0d9d63
Heidi Schellman <heidi.schellman@oregonstate.edu>
2024-09-24 16:18:41 -0700

    no 07 al9 version yet

--- gitadd.sh
+++ gitadd.sh
@@ -8,7 +8,8 @@ git add _episodes/03.3-cvmfs.md
 git add _episodes/04-intro-art-larsoft.md
 git add _episodes/04.4-mrb.md
 git add _episodes/06-larsoft-modify-module.md
-git add _episodes/07-grid-job-submission_al9.md
+#git add _episodes/07-grid-job-submission_al9.md
+#git add _episodes/07-grid-job-submission-al9.md
 git add _episodes/08-submit-jobs-w-justin.md
 git add _episodes/09-grid-batch-debug.md
 #git add _episodes/10-closing-remarks.md
